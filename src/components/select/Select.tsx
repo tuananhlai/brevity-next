3415038b3b04db4eb065d716158c7cab426e6ab7
Lai Tuan Anh <laituananh1711@gmail.com>
2025-02-24 13:20:05 +0900

    add styles

--- src/components/select/Select.tsx
+++ src/components/select/Select.tsx
@@ -1,3 +1,4 @@
+import { css } from "@emotion/react";
 import { forwardRef } from "react";
 import {
   Select as AriaSelect,
@@ -7,6 +8,16 @@ import {
   SelectValue,
 } from "react-aria-components";
 
+import {
+  borderRadiuses,
+  colors,
+  fontSizes,
+  lineHeights,
+  queries,
+  shadows,
+  spacings,
+} from "@/styles/tokens";
+import { alpha } from "@/styles/utils";
 import { ReplaceAriaRenderProps } from "@/utils";
 
 import { Popover } from "../popover";
@@ -21,11 +32,11 @@ const Select = <T extends object>(
   props: SelectProps<T>,
   ref: React.ForwardedRef<HTMLDivElement>,
 ) => {
-  const { children, items, ...rest } = props;
+  const { children, items, className = "", ...rest } = props;
   return (
-    <AriaSelect ref={ref} {...rest}>
-      <Button>
-        <SelectValue />
+    <AriaSelect ref={ref} className={`bw-select ${className}`} {...rest}>
+      <Button css={selectBtn}>
+        <SelectValue css={selectedValue} />
       </Button>
       <Popover>
         <ListBox items={items}>{children}</ListBox>
@@ -37,3 +48,77 @@ const Select = <T extends object>(
 const _Select = /*#__PURE__*/ forwardRef(Select);
 
 export { _Select as Select };
+
+const selectBtn = css`
+  background-color: transparent;
+  border: none;
+  padding: 0;
+
+  position: relative;
+  width: 100%;
+  outline: none;
+
+  &::before {
+    content: "";
+    position: absolute;
+    inset: 1px;
+    border-radius: calc(${borderRadiuses.lg} - 1px);
+    background-color: ${colors.white};
+    box-shadow: ${shadows.default};
+  }
+
+  &::after {
+    content: "";
+    position: absolute;
+    inset: 0;
+    border-radius: ${borderRadiuses.lg};
+  }
+
+  &[data-disabled] {
+    opacity: 50%;
+
+    &::before {
+      background-color: ${alpha(colors["zinc-950"], 5)};
+      box-shadow: none;
+    }
+  }
+
+  &[data-focused][data-focus-visible] {
+    &::after {
+      outline-offset: 2px;
+      outline: 2px solid ${colors["blue-500"]};
+    }
+  }
+
+  &[data-hovered] {
+    cursor: pointer;
+  }
+`;
+
+const selectedValue = css`
+  --padding-y: calc(${spacings["2.5"]} - 1px);
+  position: relative;
+  display: block;
+  width: 100%;
+  border-radius: ${borderRadiuses.lg};
+  padding-top: var(--padding-y);
+  padding-bottom: var(--padding-y);
+  padding-left: calc(${spacings["3.5"]} - 1px);
+  padding-right: calc(${spacings["7"]} - 1px);
+  min-height: 2.75rem;
+  font-size: ${fontSizes.base};
+  line-height: ${lineHeights[6]};
+  color: ${colors["zinc-950"]};
+  border: 1px solid ${alpha(colors["zinc-950"], 10)};
+
+  ${queries.sm} {
+    --padding-y: calc(${spacings["1.5"]} - 1px);
+    padding-left: calc(${spacings["3"]} - 1px);
+    min-height: 2.25rem;
+    font-size: ${fontSizes.sm};
+  }
+
+  &[data-placeholder] {
+    color: ${colors["zinc-500"]};
+  }
+`;
