b940e3c450c7e6d62186677f3d337d0708b808ae
Lai Tuan Anh <laituananh1711@gmail.com>
2025-03-06 17:57:55 +0900

    minor changes

--- src/components/ui/text/Text.tsx
+++ src/components/ui/text/Text.tsx
@@ -1,11 +1,10 @@
-import { css } from "@emotion/react";
 import { forwardRef } from "react";
 import {
   Text as AriaText,
   TextProps as AriaTextProps,
 } from "react-aria-components";
-import { colors, fontSizes, lineHeights, queries } from "@/styles/tokens";
-import { darkModeSelector } from "@/styles/utils";
+import { cn } from "@/styles/utils";
+import styles from "./Text.module.scss";
 
 export type TextProps = AriaTextProps;
 
@@ -13,24 +12,12 @@ const Text: React.ForwardRefRenderFunction<HTMLElement, TextProps> = (
   props,
   ref,
 ) => {
-  return <AriaText css={root} ref={ref} {...props} />;
+  const { className, ...rest } = props;
+  return (
+    <AriaText className={cn(styles.root, className)} ref={ref} {...rest} />
+  );
 };
 
 const _Text = /*#__PURE__*/ forwardRef(Text);
 
 export { _Text as Text };
-
-const root = css`
-  --text-color: ${colors["zinc-500"]};
-  ${darkModeSelector} {
-    --text-color: ${colors["zinc-400"]};
-  }
-
-  font-size: ${fontSizes.base};
-  line-height: ${lineHeights[6]};
-  color: var(--text-color);
-
-  ${queries.sm} {
-    font-size: ${fontSizes.sm};
-  }
-`;
