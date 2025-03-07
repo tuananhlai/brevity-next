52bf91bc4fcc3b74ad0f880f815c914927b5a328
Lại Tuấn Anh <42168740+tuananhlai@users.noreply.github.com>
2025-03-07 13:04:59 +0900

    chore/field: use scss (#16)

--- src/components/ui/field/ErrorMessage.tsx
+++ src/components/ui/field/ErrorMessage.tsx
@@ -1,34 +1,18 @@
-import { css } from "@emotion/react";
 import { FieldError, FieldErrorProps } from "react-aria-components";
-import { colors, fontSizes, lineHeights, queries } from "@/styles/tokens";
-import { darkModeSelector } from "@/styles/utils";
+import { cn } from "@/styles/utils";
+import styles from "./ErrorMessage.module.scss";
 
 export interface ErrorMessageProps extends FieldErrorProps {
   isDisabled?: boolean;
 }
 
 export const ErrorMessage: React.FC<ErrorMessageProps> = (props) => {
-  const { isDisabled, ...rest } = props;
+  const { isDisabled, className, ...rest } = props;
   return (
-    <FieldError css={root} data-disabled={isDisabled || undefined} {...rest} />
+    <FieldError
+      className={cn(styles.root, className)}
+      data-disabled={isDisabled || undefined}
+      {...rest}
+    />
   );
 };
-
-const root = css`
-  display: block;
-  font-size: ${fontSizes.base};
-  line-height: ${lineHeights[6]};
-  color: ${colors["red-600"]};
-
-  &:where([data-disabled]) {
-    opacity: 50%;
-  }
-
-  ${queries.sm} {
-    font-size: ${fontSizes.sm};
-  }
-
-  ${darkModeSelector} {
-    color: ${colors["red-500"]};
-  }
-`;
