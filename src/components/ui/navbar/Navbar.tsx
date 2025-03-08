f3c9e277bf40e9351b4543b33eae856ff516e1ca
Lai Tuan Anh <laituananh1711@gmail.com>
2025-03-08 10:14:26 +0900

    minor changes

--- src/components/ui/navbar/Navbar.tsx
+++ src/components/ui/navbar/Navbar.tsx
@@ -1,28 +1,18 @@
-import { css } from "@emotion/react";
 import { HTMLAttributes } from "react";
 import { Button, Link, LinkProps } from "react-aria-components";
-import {
-  borderRadiuses,
-  colors,
-  fontSizes,
-  fontWeights,
-  lineHeights,
-  queries,
-  spacings,
-} from "@/styles/tokens";
-import { alpha, darkModeSelector } from "@/styles/utils";
 import { TouchTarget } from "../TouchTarget";
 import { ButtonProps } from "../button";
+import styles from "./Navbar.module.scss";
 
 export interface NavbarProps extends HTMLAttributes<HTMLElement> {
   children: React.ReactNode;
 }
 
 export const Navbar: React.FC<NavbarProps> = (props) => {
-  return <nav css={root} {...props} />;
+  return <nav className={styles.root} {...props} />;
 };
 
-export const NavbarSpacer = () => <div css={spacer} />;
+export const NavbarSpacer = () => <div className={styles.spacer} />;
 
 export interface NavbarLinkProps extends LinkProps {
   style?: React.CSSProperties;
@@ -33,7 +23,7 @@ export interface NavbarLinkProps extends LinkProps {
 export const NavbarLink: React.FC<NavbarLinkProps> = (props) => {
   const { children, ...rest } = props;
   return (
-    <Link css={item} {...rest}>
+    <Link className={styles.item} {...rest}>
       <TouchTarget>{children}</TouchTarget>
     </Link>
   );
@@ -49,60 +39,8 @@ export interface NavbarButtonProps
 export const NavbarButton: React.FC<NavbarButtonProps> = (props) => {
   const { children, ...rest } = props;
   return (
-    <Button css={item} {...rest}>
+    <Button className={styles.item} {...rest}>
       <TouchTarget>{children}</TouchTarget>
     </Button>
   );
 };
-
-const root = css`
-  --navbar-gap: ${spacings[4]};
-  width: 100%;
-  display: flex;
-  align-items: center;
-  gap: var(--navbar-gap);
-  padding: 0 ${spacings["2.5"]};
-`;
-
-const spacer = css`
-  flex: 1;
-`;
-
-const item = css`
-  display: flex;
-  min-width: 0;
-  align-items: center;
-  border-radius: ${borderRadiuses.lg};
-  padding: ${spacings[2]};
-  text-align: left;
-  font-size: ${fontSizes.base};
-  line-height: ${lineHeights[6]};
-  font-weight: ${fontWeights.medium};
-  color: ${colors["zinc-950"]};
-  text-decoration: none;
-  background-color: transparent;
-  border: none;
-  --navbar-item-hover-bg-color: ${alpha(colors["zinc-950"], 5)};
-
-  ${queries.sm} {
-    font-size: ${fontSizes.sm};
-    line-height: ${lineHeights[5]};
-  }
-
-  &:where(:not([data-focus-visible])) {
-    outline: none;
-  }
-
-  &:where([data-hovered]) {
-    cursor: pointer;
-  }
-
-  &:where([data-hovered], [data-pressed]) {
-    background-color: var(--navbar-item-hover-bg-color);
-  }
-
-  ${darkModeSelector} {
-    --navbar-item-hover-bg-color: ${alpha(colors.white, 5)};
-    color: ${colors.white};
-  }
-`;
