c46906b6a474fde70c47d78ef710e9f5fccf2db3
Lai Tuan Anh <laituananh1711@gmail.com>
2025-03-24 13:37:42 +0900

    minor changes

--- src/components/ui/tooltip/Tooltip.test.tsx
+++ src/components/ui/tooltip/Tooltip.test.tsx
@@ -1,3 +1,23 @@
-it("should work", () => {
-  expect(1 + 1).toBe(2);
+import { act, render, screen } from "@testing-library/react";
+import { Button, TooltipTrigger } from "react-aria-components";
+import { LuPencil } from "react-icons/lu";
+import { Tooltip } from "./Tooltip";
+
+it("should display tooltip when trigger is focused", () => {
+  render(
+    <TooltipTrigger>
+      <Button>
+        <LuPencil />
+      </Button>
+      <Tooltip>Tooltip content</Tooltip>
+    </TooltipTrigger>,
+  );
+
+  const button = screen.getByRole("button");
+  act(() => {
+    button.focus();
+  });
+
+  expect(button).toHaveAccessibleDescription("Tooltip content");
+  expect(screen.getByText("Tooltip content")).toBeInTheDocument();
 });
