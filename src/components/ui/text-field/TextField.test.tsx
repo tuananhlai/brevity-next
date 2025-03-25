57a9cd966e048c13c8c23165094843d5d8b0be73
Tuan Anh Lai <tuananh.lai@xaiondata.co.jp>
2025-03-25 09:46:20 +0900

    chore/text-field: add unit tests

--- src/components/ui/text-field/TextField.test.tsx
+++ src/components/ui/text-field/TextField.test.tsx
@@ -1,3 +1,45 @@
-it("should work", () => {
-  expect(1 + 1).toBe(2);
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import { TextField, TextFieldProps } from "./TextField";
+
+describe("TextField", () => {
+  it("should have the correct role", () => {
+    renderTextField();
+    expect(screen.getByRole("textbox")).toBeInTheDocument();
+  });
+
+  it("should be associated with a label", () => {
+    render(<TextField label="Label" />);
+    expect(screen.getByLabelText(/label/i)).toBeInTheDocument();
+  });
+
+  it("should be described by a description when `description` prop is provided", () => {
+    renderTextField({ description: "Description" });
+    expect(screen.getByRole("textbox")).toHaveAccessibleDescription(
+      "Description",
+    );
+  });
+
+  it("should be described by an error message when `isInvalid` and `errorMessage` props are provided", () => {
+    renderTextField({ isInvalid: true, errorMessage: "Error" });
+    expect(screen.getByRole("textbox")).toHaveAccessibleDescription("Error");
+  });
+
+  it("should be required when `isRequired` prop is provided", () => {
+    renderTextField({ isRequired: true });
+    expect(screen.getByRole("textbox")).toBeRequired();
+  });
+
+  it("should invoke `onChange` prop when the value changes", async () => {
+    const onChange = jest.fn();
+    renderTextField({ onChange });
+
+    await userEvent.type(screen.getByRole("textbox"), "test");
+
+    expect(onChange).toHaveBeenCalledWith("test");
+  });
 });
+
+const renderTextField = (props?: TextFieldProps) => {
+  return render(<TextField aria-label="label" {...props} />);
+};
--- src/features/sign-in/components/sign-in-form/SignInForm.test.tsx
+++ src/features/sign-in/components/sign-in-form/SignInForm.test.tsx
@@ -1,3 +1,66 @@
-it("should work", () => {
-  expect(1 + 1).toBe(2);
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import { SignInForm, SignInFormProps } from "./SignInForm";
+
+describe("SignInForm", () => {
+  it("should submit", async () => {
+    const onSubmit = jest.fn();
+    const email = "test@test.com";
+    const password = "test";
+    renderSignInForm({ onSubmit });
+
+    await userEvent.type(
+      screen.getByRole("textbox", { name: /email/i }),
+      email,
+    );
+    await userEvent.type(screen.getByLabelText(/password/i), password);
+    await userEvent.click(screen.getByRole("button", { name: "Sign in" }));
+
+    expect(onSubmit).toHaveBeenCalledWith({
+      email,
+      password,
+    });
+  });
+
+  it("email input should be invalid when the email format is incorrect", async () => {
+    renderSignInForm();
+
+    const emailInput = screen.getByRole("textbox", { name: /email/i });
+    const submitButton = screen.getByRole("button", { name: "Sign in" });
+
+    // Empty email.
+    await userEvent.click(submitButton);
+
+    expect(emailInput).toBeInvalid();
+
+    // Invalid email format.
+    await userEvent.type(emailInput, "invalid");
+    await userEvent.click(submitButton);
+
+    expect(emailInput).toBeInvalid();
+  });
+
+  it("password input should be invalid when the password is too short", async () => {
+    renderSignInForm();
+
+    const passwordInput = screen.getByLabelText(/password/i);
+    const submitButton = screen.getByRole("button", { name: "Sign in" });
+
+    // Empty password.
+    await userEvent.click(submitButton);
+
+    expect(passwordInput).toBeInvalid();
+  });
 });
+
+const renderSignInForm = (props?: SignInFormProps) => {
+  const { id = "sign-in-form", ...rest } = props ?? {};
+  return render(
+    <div>
+      <SignInForm id={id} {...rest} />
+      <button form={id} type="submit">
+        Sign in
+      </button>
+    </div>,
+  );
+};
