fea4809f78ce35a64d2c5a41edb1755a91c726ef
tuananhlai <42168740+tuananhlai@users.noreply.github.com>
2025-04-14 09:38:48 +0900

    minor changes

--- src/app/pages/home-page/HomePage.tsx
+++ src/app/pages/home-page/HomePage.tsx
@@ -4,7 +4,7 @@ import { StackedLayout } from "@/components/stacked-layout";
 import { LinkButton } from "@/components/ui/button";
 import { Heading } from "@/components/ui/text";
 import { useGetArticlePreviews } from "@/features/home/api/getArticlePreviews";
-import { BlogCard, BlogCardProps } from "@/features/home/components/blog-card";
+import { BlogCard } from "@/features/home/components/blog-card";
 import styles from "./HomePage.module.scss";
 
 export const HomePage: NextPage = () => {
@@ -44,62 +44,3 @@ export const HomePage: NextPage = () => {
     </StackedLayout>
   );
 };
-
-const mockData: BlogCardProps[] = [
-  {
-    author: {
-      avatarURL:
-        "https://gravatar.com/avatar/515c8955d83d1a52b0e253576916b991?s=400&d=robohash&r=x",
-      name: "Author Name",
-      position: "Author Position",
-    },
-    authorHref: "/author",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iste commodi iure itaque reprehenderit tenetur reiciendis sint asperiores quos aperiam cupiditate? Natus voluptates exercitationem eius eveniet asperiores reiciendis delectus ipsum quos!",
-    href: "/blog",
-    publishedAt: new Date(2021, 1, 1),
-    title: "Blog Title",
-  },
-  {
-    author: {
-      avatarURL:
-        "https://gravatar.com/avatar/515c8955d83d1a52b0e253576916b991?s=400&d=robohash&r=x",
-      name: "Author Name",
-      position: "Author Position",
-    },
-    authorHref: "/author",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iste commodi iure itaque reprehenderit tenetur reiciendis sint asperiores quos aperiam cupiditate? Natus voluptates exercitationem eius eveniet asperiores reiciendis delectus ipsum quos!",
-    href: "/blog",
-    publishedAt: new Date(2021, 1, 30),
-    title: "Blog Title",
-  },
-  {
-    author: {
-      avatarURL:
-        "https://gravatar.com/avatar/515c8955d83d1a52b0e253576916b991?s=400&d=robohash&r=x",
-      name: "Author Name",
-      position: "Author Position",
-    },
-    authorHref: "/author",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iste commodi iure itaque reprehenderit tenetur reiciendis sint asperiores quos aperiam cupiditate? Natus voluptates exercitationem eius eveniet asperiores reiciendis delectus ipsum quos!",
-    href: "/blog",
-    publishedAt: new Date(2021, 1, 30),
-    title: "Blog Title",
-  },
-  {
-    author: {
-      avatarURL:
-        "https://gravatar.com/avatar/515c8955d83d1a52b0e253576916b991?s=400&d=robohash&r=x",
-      name: "Author Name",
-      position: "Author Position",
-    },
-    authorHref: "/author",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iste commodi iure itaque reprehenderit tenetur reiciendis sint asperiores quos aperiam cupiditate? Natus voluptates exercitationem eius eveniet asperiores reiciendis delectus ipsum quos!",
-    href: "/blog",
-    publishedAt: new Date(2021, 1, 30),
-    title: "Blog Title",
-  },
-];
