7c0d5dec99964c7be282c1663d668262cd188db8
tuananhlai <42168740+tuananhlai@users.noreply.github.com>
2025-05-21 20:14:55 +0900

    chore/select: add some tests

--- src/components/ui/select/Select.test.tsx
+++ src/components/ui/select/Select.test.tsx
@@ -1,3 +1,53 @@
-it("should work", () => {
-  expect(1 + 1).toBe(2);
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import { Select, SelectItem, SelectProps } from "@/components/ui/select";
+
+it("should have the correct roles", async () => {
+  render(<ExampleSelect />);
+
+  await userEvent.click(screen.getByRole("button"));
+
+  expect(screen.getByRole("listbox")).toBeInTheDocument();
+  expect(screen.getAllByRole("option")).toHaveLength(3);
 });
+
+it("should be open by default when `defaultOpen` is true", () => {
+  render(<ExampleSelect defaultOpen />);
+
+  expect(screen.getByRole("listbox")).toBeInTheDocument();
+});
+
+it("should close the listbox when the user clicks outside", async () => {
+  render(<ExampleSelect defaultOpen />);
+
+  await userEvent.click(document.body);
+
+  expect(screen.queryByRole("listbox")).not.toBeInTheDocument();
+});
+
+it("should have label and description when provided", () => {
+  render(<ExampleSelect label="label" description="description" />);
+
+  expect(screen.getByRole("button")).toHaveAccessibleName(/label/);
+  expect(screen.getByRole("button")).toHaveAccessibleDescription(/description/);
+});
+
+it("should be associated with an error message when `isInvalid` is true", () => {
+  render(
+    <ExampleSelect isInvalid errorMessage="error" description="description" />,
+  );
+
+  expect(screen.getByRole("button")).toHaveAccessibleDescription(
+    "description error",
+  );
+});
+
+const ExampleSelect = (props: Partial<SelectProps<object>>) => {
+  return (
+    <Select aria-label="example" {...props}>
+      <SelectItem>Option 1</SelectItem>
+      <SelectItem>Option 2</SelectItem>
+      <SelectItem>Option 3</SelectItem>
+    </Select>
+  );
+};
