65e5874ba533d8d8e887422e041ef3ce4c6d2276
tuananhlai <42168740+tuananhlai@users.noreply.github.com>
2025-07-05 11:43:04 +0900

    minor changes to table

--- src/components/ui/table/Table.stories.tsx
+++ src/components/ui/table/Table.stories.tsx
@@ -1,4 +1,5 @@
 import { Meta, StoryObj } from "@storybook/nextjs";
+import { useState } from "react";
 import {
   Table,
   TableBody,
@@ -78,17 +79,84 @@ export const Selection: Story = {
   },
 };
 
+export const Overflow: Story = {
+  parameters: {
+    layout: "centered",
+  },
+  render: () => {
+    const [isContrained, setIsContrained] = useState(true);
+
+    return (
+      <>
+        <button onClick={() => setIsContrained(!isContrained)}>
+          Toggle constrained
+        </button>
+        <div
+          style={
+            isContrained
+              ? { display: "flex", width: "600px", height: "400px" }
+              : {}
+          }
+        >
+          <Table>
+            <TableHeader>
+              <TableColumn>Name</TableColumn>
+              <TableColumn>Email</TableColumn>
+              <TableColumn>Phone</TableColumn>
+              <TableColumn>Address</TableColumn>
+              <TableColumn>City</TableColumn>
+              <TableColumn>State</TableColumn>
+              <TableColumn>Zip</TableColumn>
+              <TableColumn>Country</TableColumn>
+              <TableColumn style={{ width: "var(--bw-space-48)" }}>
+                Note
+              </TableColumn>
+            </TableHeader>
+            <TableBody items={sampleRows}>
+              {(item) => (
+                <TableRow key={item.id}>
+                  <TableCell>{item.name}</TableCell>
+                  <TableCell>{item.email}</TableCell>
+                  <TableCell>{item.phone}</TableCell>
+                  <TableCell>{item.address}</TableCell>
+                  <TableCell>{item.city}</TableCell>
+                  <TableCell>{item.state}</TableCell>
+                  <TableCell>{item.zip}</TableCell>
+                  <TableCell>{item.country}</TableCell>
+                  <TableCell>{item.note}</TableCell>
+                </TableRow>
+              )}
+            </TableBody>
+          </Table>
+        </div>
+      </>
+    );
+  },
+};
+
 const sampleRows = [
   {
     id: 1,
     name: "John Doe",
     email: "john.doe@example.com",
     phone: "123-456-7890",
+    address: "123 Main St",
+    city: "Anytown",
+    state: "CA",
+    zip: "12345",
+    country: "USA",
+    note: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
   },
   {
     id: 2,
     name: "Jane Doe",
     email: "jane.doe@example.com",
     phone: "123-456-7890",
+    address: "123 Main St",
+    city: "Anytown",
+    state: "CA",
+    zip: "12345",
+    country: "USA",
+    note: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
   },
 ];
