d185d084dec1f17f9ad3a1509cc90674a5072a37
Lai Tuan Anh <42168740+tuananhlai@users.noreply.github.com>
2025-07-15 18:32:53 +0700

    minor changes

--- src/app/pages/manage-api-key/ManageAPIKey.tsx
+++ src/app/pages/manage-api-key/ManageAPIKey.tsx
@@ -1,6 +1,7 @@
 import { msg } from "@lingui/macro";
 import { useLingui } from "@lingui/react";
 import { NextPage } from "next";
+import Head from "next/head";
 import { DialogTrigger } from "react-aria-components";
 import { StudioLayout } from "@/components/studio-layout";
 import { Button } from "@/components/ui/button";
@@ -8,27 +9,34 @@ import { Flex } from "@/components/ui/layout";
 import { Heading, Text } from "@/components/ui/text";
 import { AddApiKeyDialog } from "@/features/digital-author/components/add-api-key-dialog";
 import { ManageAPIKeyTable } from "@/features/digital-author/components/manage-api-key-table";
+import { getPageTitle } from "@/utils/misc";
 import styles from "./ManageAPIKey.module.scss";
 
 export const ManageAPIKey: NextPage = () => {
   const { _ } = useLingui();
+  const pageHeading = _(msg`Manage API keys`);
 
   return (
-    <StudioLayout>
-      <Heading level={1}>{_(msg`Manage API keys`)}</Heading>
-      <Text className={styles.pageDescription}>
-        {_(
-          msg`Register your LLM provider API keys in order to create a new digital author.`,
-        )}
-      </Text>
+    <>
+      <Head>
+        <title>{getPageTitle(pageHeading)}</title>
+      </Head>
+      <StudioLayout>
+        <Heading level={1}>{pageHeading}</Heading>
+        <Text className={styles.pageDescription}>
+          {_(
+            msg`Register your LLM provider API keys in order to create a new digital author.`,
+          )}
+        </Text>
 
-      <Flex style={{ marginTop: "var(--bw-space-4)" }} justify="end">
-        <DialogTrigger>
-          <Button>{_(msg`Create new`)}</Button>
-          <AddApiKeyDialog />
-        </DialogTrigger>
-      </Flex>
-      <ManageAPIKeyTable className={styles.apiKeyTable} items={[]} />
-    </StudioLayout>
+        <Flex style={{ marginTop: "var(--bw-space-4)" }} justify="end">
+          <DialogTrigger>
+            <Button>{_(msg`Create new`)}</Button>
+            <AddApiKeyDialog />
+          </DialogTrigger>
+        </Flex>
+        <ManageAPIKeyTable className={styles.apiKeyTable} items={[]} />
+      </StudioLayout>
+    </>
   );
 };
