6965c2a280e8c0e77413614ca14c317ace95fc34
Lai Tuan Anh <42168740+tuananhlai@users.noreply.github.com>
2026-03-08 17:47:19 +0900

    chore: move Home page component to routes/

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,48 @@
+import { Trans } from "@lingui/react/macro";
 import { createFileRoute } from "@tanstack/react-router";
-import { Home } from "@/app/pages/home";
+import { StackedLayout } from "@/components/stacked-layout";
+import { Heading } from "@/components/ui/text";
+import { useGetArticlePreviews } from "@/features/home/api/getArticlePreviews";
+import { ArticleCard } from "@/features/home/components/article-card";
+import styles from "./index.module.scss";
 
 export const Route = createFileRoute("/")({
-  component: Home,
+  component: RouteComponent,
 });
+
+function RouteComponent() {
+  const { data = { items: [] } } = useGetArticlePreviews();
+
+  return (
+    <StackedLayout className={styles.root}>
+      <div className={styles.main}>
+        <section className={styles.section}>
+          <Heading level={2}>
+            <Trans>Newest posts</Trans>
+          </Heading>
+          <div className={styles.blogs}>
+            {data.items.map((v) => (
+              <div key={v.id} className={styles.blogCard}>
+                <ArticleCard
+                  author={{
+                    name: v.author.displayName ?? v.author.username,
+                    avatarURL: getDefaultAvatarURL(v.author.id),
+                  }}
+                  authorHref={v.author.username}
+                  description={v.description}
+                  href={`/article/${v.slug}`}
+                  publishedAt={new Date(v.createdAt)}
+                  title={v.title}
+                />
+              </div>
+            ))}
+          </div>
+        </section>
+      </div>
+    </StackedLayout>
+  );
+}
+
+const getDefaultAvatarURL = (authorID: string): string => {
+  return `https://api.dicebear.com/9.x/glass/jpg?seed=${authorID}`;
+};
